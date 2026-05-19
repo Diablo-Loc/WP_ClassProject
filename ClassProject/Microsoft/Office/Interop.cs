0c4d1bd726c1b36167d177e45ecd3c924114d1f9
Diablo-Loc <ndakloc@gmail.com>
2026-05-19 13:30:17 +0700

    Delete Interop.cs

--- ClassProject/Microsoft/Office/Interop.cs
+++
@@ -1,4 +0,0 @@
-﻿// This file was accidentally added and conflicts with the real Microsoft.Office.Interop namespace.
-// Remove the placeholder types to allow use of Microsoft.Office.Interop from NuGet/COM.
-
-// Intentionally left empty.
